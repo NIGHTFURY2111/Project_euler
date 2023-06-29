7aecbfa4cb56a24818ff4bfd4131370bb09b557f
NIGHTFURY2111 <hhimanshsaraswat@gmail.com>
2023-06-29 20:02:55 +0530

    Solved Question 19

--- project euler/19- Counting Sundays.py
+++ project euler/19- Counting Sundays.py
@@ -1,10 +1,12 @@
 import calendar
 
 print(
-    sum(list(
-        1 
-        for i in range(1901,2000+1)
-        for j in range(1,12+1)
-        if calendar.weekday(i,j,1) == 6
-        ))
+    sum(
+        list(
+            1
+            for i in range(1901,2000+1)
+            for j in range(1,12+1)
+            if calendar.weekday(i,j,1) == 6
+            )
+        )
     )
