e474f2c3cd704877729b2fc95e760a3a14b49a7e
NIGHTFURY2111 <65185393+NIGHTFURY2111@users.noreply.github.com>
2023-07-04 23:00:55 +0530

    solved Q21 and made Q19 even smaller :)

---
+++ project euler/21- Amicable Numbers.py
@@ -0,0 +1,15 @@
+list = {}
+ambi_nums = []
+for i in range(1,300):
+    temp = []
+    for j in range (1,i):
+        if i%j ==0:
+            temp.append(j)
+    list[i] = sum(temp)
+total = 0
+for key, val in list.items():
+    if val in list.keys() and list[val] == key and key !=val:
+        ambi_nums.append(key)
+
+
+print(sum(total))
